8eec1712d138fc4deb2c44c79a1b5cb10e427845
Szymon Pajchrowski <szymon.pajchrowski@conclusive.pl>
2023-04-11 10:25:49 +0200

    tools/i2c_wrapper: fixed reading method that causes occasional link-down result
    Tested by:
    1. regression tests using selftest-tool
    
      root@hgx:/tmp# cat report12312.json | grep -i i2cdirect -c
      28
      root@hgx:/tmp# cat report12312.json | grep -i i2cdirect -A 1 | grep -i link-up -c
      28
    
    2. extended tester script file, copied onto and ran on hw
    
    baseline before fix
    
      root@hgx:/tmp# ./i2c_wrapper_test.sh -y
      Tests total 56 passed 34 failed 22
    
    after fix
    
      root@hgx:/tmp# ./i2c_wrapper_test.sh -y
      Tests total 56 passed 56 failed 0
    
    Fixes nvbug https://nvbugswb.nvidia.com/NvBugs5/SWBug.aspx?bugid=4014983&cmtNo=
    Signed-off-by: Szymon Pajchrowski <szymon.pajchrowski@conclusive.pl>

--- tools/wrapper_i2c_status/i2c_wrapper_test.sh
+++ tools/wrapper_i2c_status/i2c_wrapper_test.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
+if [[ "$1" != "-y" ]]; then
+    echo "Are you running this test script on hw machine? [y/n]"
+    read userInput
+    if [[ "$userInput" != "y" ]]; then
+        echo "it has to be run on hw, exiting"
+        exit 1
+    fi
+fi
+
 testsTotal=0
 testsPassed=0
 testsFailed=0
 
-scriptName="i2c_wrapper"
-# export for dependant map tool, you might need to set it yourself if doesn't work
-export DEVICE_MAP_FILE="../device_id_mapping/device_id_map.csv"
-export DEVICE_NAME_MAPPER="../device_id_mapping/device-id-norm.sh"
+scriptName="./i2c_wrapper"
+export DEVICE_NAME_MAPPER="device-id-norm.sh"
+ITER_CFG=500
 
 perfTest() 
 {
@@ -17,16 +25,21 @@ perfTest()
     local artOut="$3"
     local rcCode=$4
     local exp="$5"
+    local optional_compare_override_rc="$6"
 
     # workaround to bash quotes stripping
     artOut=${artOut//' '/'_'}
 
-    local cmd="./$scriptName -test-run $symbol $dev $artOut $rcCode"
+    local cmd="$scriptName -test-run $symbol $dev $artOut $rcCode"
     local resp=$($cmd);
 
     # workaround - do second quiet run just to catch rc code, above always rets 0
     $cmd 1> /dev/null 2> /dev/null
     local cmdRc=$?
+
+    if [[ -n $optional_compare_override_rc ]]; then
+        rcCode=$optional_compare_override_rc
+    fi
 
     if [ "$resp" != "$exp" ] || [ $rcCode -ne $cmdRc ]; then
         testsFailed=$(($testsFailed + 1))
@@ -47,6 +60,58 @@ perfTest()
     echo -n -e "test $testsTotal\r"
 }
 
+errorRateTest() 
+{
+    testsTotal=$(($testsTotal + 1))
+    local ITER_TO_FINISH=$ITER_CFG
+    local ITER=0
+    local SUCC=0
+    local FAIL=0
+    local CMD="$scriptName i2c_access $1"
+
+    echo "start: $(date) using cmd \"$CMD\" iter cnt $ITER_TO_FINISH"
+
+    while true
+    do
+        OUTPUT=$($CMD)
+        ORIGINAL_RC=$?
+
+        if  [[ "$OUTPUT" == *"link-up"* ]]; then
+            RC=0
+        else
+            RC=1
+        fi
+
+        ITER=$((ITER+1))
+
+        if [ $RC -ne 0 ]; then
+            FAIL=$((FAIL+1))
+            echo "ERROR: iter $ITER succ $SUCC fail $FAIL cmd \"$CMD\" out \"$OUTPUT\" rc \"$ORIGINAL_RC\""
+        else
+            SUCC=$((SUCC+1))
+        fi
+
+        if (( ITER % 100 == 0 )); then
+            echo "progress: $(date) current iter $ITER / $ITER_TO_FINISH success $SUCC fail $FAIL"
+        fi
+
+        if (( ITER >= ITER_TO_FINISH )); then
+            echo "finished: $(date) current iter $ITER success $SUCC fail $FAIL"
+            break
+        fi
+
+    done
+
+    if [ $FAIL -ne 0 ]; then
+        testsFailed=$(($testsFailed + 1))
+        echo "ERROR: error rate test failed, iter $ITER success $SUCC fail $FAIL"
+    else
+        testsPassed=$(($testsPassed + 1))
+    fi
+    
+    echo -n -e "test $testsTotal\r"
+}
+
 summary()
 {
     echo "Tests total $testsTotal passed $testsPassed failed $testsFailed"
@@ -63,11 +128,17 @@ summary()
 ###########################################################
 # bmc uses different method (detect instead of get)
 perfTest "i2c_access" "BMC_0" "<dummy>" 0 $'link-up'
-perfTest "i2c_access" "BMC_0" "<dummy>" 1 $'link-down; rc 1'
+perfTest "i2c_access" "BMC_0" "<dummy>" 1 $'link-down; rc 100; <dummy>' 0
 
 perfTest "i2c_access" "GPU_SXM_4" "0x11" 0 $'link-up'
 perfTest "i2c_access" "GPU_SXM_0" " " 1 "Error: cannot map deviceID (GPU_SXM_0)"
-perfTest "i2c_access" "GPU_SXM_4" "Error: Read failed" 1 $'link-down; rc 1'
+perfTest "i2c_access" "GPU_SXM_4" "Error: Read failed" 1 $'link-down; rc 100; error: read failed' 0
+
+perfTest "i2c_access" "GPU_SXM_4" "Error: Sending messages failed: Protocol error" 1 $'link-down; rc 101' 0
+perfTest "i2c_access" "GPU_SXM_4" "Error: Sending messages failed: No such device or address" 1 $'' 2
+perfTest "i2c_access" "GPU_SXM_4" "Error: Sending messages failed: Input/output error" 1 $'link-down; rc 102' 0
+perfTest "i2c_access" "GPU_SXM_4" "Error: Sending messages failed: Connection timed out" 1 $'link-down; rc 103' 0
+perfTest "i2c_access" "GPU_SXM_4" "Error: Sending messages failed: unknown error" 1 $'link-down; rc 100; error: sending messages failed: unknown error' 0
 
 perfTest "i2c_access" "GPU_SXM_1"      "0x11" 0 $'link-up'
 perfTest "i2c_access" "GPU_SXM_2"      "0x11" 0 $'link-up'
@@ -93,6 +164,31 @@ perfTest "i2c_access" "PCIeSwitch_0"   "0x11" 0 $'link-up'
 perfTest "i2c_access" "FPGA_0"         "0x11" 0 $'link-up'
 perfTest "i2c_access" "BMC_0"          "0x11" 0 $'link-up'
 
+echo ""
+
+errorRateTest "GPU_SXM_1"
+errorRateTest "GPU_SXM_2"
+errorRateTest "GPU_SXM_3"
+errorRateTest "GPU_SXM_4"
+errorRateTest "GPU_SXM_5"
+errorRateTest "GPU_SXM_6"
+errorRateTest "GPU_SXM_7"
+errorRateTest "GPU_SXM_8"
+errorRateTest "NVSwitch_0"
+errorRateTest "NVSwitch_1"
+errorRateTest "NVSwitch_2"
+errorRateTest "NVSwitch_3"
+errorRateTest "PCIeRetimer_0"
+errorRateTest "PCIeRetimer_1"
+errorRateTest "PCIeRetimer_2"
+errorRateTest "PCIeRetimer_3"
+errorRateTest "PCIeRetimer_4"
+errorRateTest "PCIeRetimer_5"
+errorRateTest "PCIeRetimer_6"
+errorRateTest "PCIeRetimer_7"
+errorRateTest "PCIeSwitch_0"
+errorRateTest "FPGA_0"
+errorRateTest "BMC_0"
 ###########################################################
 
 summary
